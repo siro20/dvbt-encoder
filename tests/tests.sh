e207b054cc1b38f43537ed14f904f12332afe916
siro <siro@das-labor.org>
2014-05-10 08:16:04 +0200

    add more tests

--- tests/tests.sh
+++ tests/tests.sh
@@ -2,82 +2,170 @@
 make all
 LD_LIBRARY_PATH=../libdvbtenc/lib
 export LD_LIBRARY_PATH
-cat ./ref_2K_4QAM_1_2_32/test.m2v | ./ed -o 2048 -b 8 -c 2 -g 32 -m 4 -a 1 -s 0 > ./output_ed_cmp.bin
-if cmp -n $(stat -c %s "./ref_2K_4QAM_1_2_32/output_ed.bin") ./ref_2K_4QAM_1_2_32/output_ed.bin ./output_ed_cmp.bin ; then
-  echo pbrs same
+arguments="-o 2048 -b 8 -c 2 -g 32 -m 2 -a 1 -s 0"
+folderpath="ref_2K_4QAM_1_2_32"
+echo "****************"
+echo "testing 2K mode, bandwidth 8Mhz, coderate 1/2, guardinterval 1/32, modulation qpsk, alpha 1, outputformat float"
+cat ./$folderpath/test.m2v | ./ed $arguments > ./output_ed_cmp.bin
+if cmp -n $(stat -c %s "./$folderpath/output_ed.bin") ./$folderpath/output_ed.bin ./output_ed_cmp.bin ; then
+  echo ed same
 else
-  echo pbrs different
+  echo ed different
 fi
 rm ./output_ed_cmp.bin
 
-cat ./ref_2K_4QAM_1_2_32/output_ed.bin | ./rs -o 2048 -b 8 -c 2 -g 32 -m 4 -a 1 -s 0 > ./output_rs_cmp.bin
-if cmp -n $(stat -c %s "./ref_2K_4QAM_1_2_32/output_rs.bin") -b ./ref_2K_4QAM_1_2_32/output_rs.bin ./output_rs_cmp.bin; then
+cat ./$folderpath/output_ed.bin | ./rs $arguments > ./output_rs_cmp.bin
+if cmp -n $(stat -c %s "./$folderpath/output_rs.bin") -b ./$folderpath/output_rs.bin ./output_rs_cmp.bin; then
   echo rs same
 else
   echo rs different
 fi
 rm ./output_rs_cmp.bin
 
-cat ./ref_2K_4QAM_1_2_32/output_rs.bin | ./oi -o 2048 -b 8 -c 2 -g 32 -m 4 -a 1 -s 0 > ./output_ci_cmp.bin
-if cmp -n $(stat -c %s "./ref_2K_4QAM_1_2_32/output_ci.bin") -b ./ref_2K_4QAM_1_2_32/output_ci.bin ./output_ci_cmp.bin ; then
+cat ./$folderpath/output_rs.bin | ./oi $arguments > ./output_ci_cmp.bin
+if cmp -n $(stat -c %s "./$folderpath/output_ci.bin") -b ./$folderpath/output_ci.bin ./output_ci_cmp.bin ; then
   echo ci same
 else
   echo ci different
 fi
 rm ./output_ci_cmp.bin
 
-cat ./ref_2K_4QAM_1_2_32/output_ci.bin | ./ce -o 2048 -b 8 -c 2 -g 32 -m 2 -a 1 -s 0 > ./output_ve_cmp.bin
-if cmp -n $(stat -c %s "./ref_2K_4QAM_1_2_32/output_ve.bin") -b ./ref_2K_4QAM_1_2_32/output_pu.bin ./output_ve_cmp.bin ; then
-  echo ce same
+cat ./$folderpath/output_ci.bin | ./ce $arguments > ./output_pu_cmp.bin
+if cmp -n $(stat -c %s "./$folderpath/output_pu.bin") -b ./$folderpath/output_pu.bin ./output_pu_cmp.bin ; then
+  echo ce+puncturing same
 else
-  echo ce different
+  echo ce+puncturing different
 fi
-rm ./output_ve_cmp.bin
+rm ./output_pu_cmp.bin
 
-cat ./ref_2K_4QAM_1_2_32/output_pu.bin | ./ii -o 2048 -b 8 -c 2 -g 32 -m 2 -a 1 -s 0 > ./output_bi_cmp.bin
-if cmp -b -n $(stat -c %s "./ref_2K_4QAM_1_2_32/output_bi.bin") -b ./ref_2K_4QAM_1_2_32/output_bi.bin ./output_bi_cmp.bin ; then
+cat ./$folderpath/output_pu.bin | ./ii $arguments > ./output_bi_cmp.bin
+if cmp -b -n $(stat -c %s "./$folderpath/output_bi.bin") -b ./$folderpath/output_bi.bin ./output_bi_cmp.bin ; then
   echo bi same
 else
   echo bi different
 fi
 rm ./output_bi_cmp.bin
 
-cat ./ref_2K_4QAM_1_2_32/output_bi.bin | ./si -o 2048 -b 8 -c 2 -g 32 -m 2 -a 1 -s 0 > ./output_si_cmp.bin
-if cmp -b -n $(stat -c %s "./ref_2K_4QAM_1_2_32/output_si.bin") -b ./ref_2K_4QAM_1_2_32/output_si.bin ./output_si_cmp.bin ; then
+cat ./$folderpath/output_bi.bin | ./si $arguments > ./output_si_cmp.bin
+if cmp -b -n $(stat -c %s "./$folderpath/output_si.bin") -b ./$folderpath/output_si.bin ./output_si_cmp.bin ; then
   echo si same
 else
   echo si different
 fi
 rm ./output_si_cmp.bin
 
-cat ./ref_2K_4QAM_1_2_32/output_si.bin | ./sm -o 2048 -b 8 -c 2 -g 32 -m 2 -a 1 -s 0 > ./output_ma_cmp.bin
-if cmp -b -n $(stat -c %s "./ref_2K_4QAM_1_2_32/output_ma.bin") -b ./ref_2K_4QAM_1_2_32/output_ma.bin ./output_ma_cmp.bin ; then
+cat ./$folderpath/output_si.bin | ./sm $arguments > ./output_ma_cmp.bin
+if cmp -b -n $(stat -c %s "./$folderpath/output_ma.bin") -b ./$folderpath/output_ma.bin ./output_ma_cmp.bin ; then
   echo ma same
 else
   echo ma different
 fi
 rm ./output_ma_cmp.bin
 
-cat ./ref_2K_4QAM_1_2_32/output_ma.bin | ./chan -o 2048 -b 8 -c 2 -g 32 -m 2 -a 1 -s 0 > ./output_fd_cmp.bin
-if cmp -b -n $(stat -c %s "./ref_2K_4QAM_1_2_32/output_fd.bin") -b ./ref_2K_4QAM_1_2_32/output_fd.bin ./output_fd_cmp.bin ; then
+cat ./$folderpath/output_ma.bin | ./chan $arguments > ./output_fd_cmp.bin
+if cmp -b -n $(stat -c %s "./$folderpath/output_fd.bin") -b ./$folderpath/output_fd.bin ./output_fd_cmp.bin ; then
   echo chan same
 else
   echo chan different
 fi
 rm ./output_fd_cmp.bin
 
-cat ./ref_2K_4QAM_1_2_32/output_fd.bin | ./ifft -o 2048 -b 8 -c 2 -g 32 -m 2 -a 1 -s 0 > ./output_cmp.bin
-if ./float_cmp ./ref_2K_4QAM_1_2_32/output.bin ./output_cmp.bin 0.25 ; then
+cat ./$folderpath/output_fd.bin | ./ifft $arguments > ./output_cmp.bin
+if ./float_cmp ./$folderpath/output.bin ./output_cmp.bin 0.25 ; then
   echo ifft same
 else
   echo ifft different
 fi
 rm ./output_cmp.bin
 
-cat ./ref_2K_4QAM_1_2_32/test.m2v |./dvbtenc -o 2048 -b 8 -c 2 -g 32 -m 2 -a 1 -s 0 > ./output.bin
-if cmp -b -n $(stat -c %s "./ref_2K_4QAM_1_2_32/output.bin") -b ./ref_2K_4QAM_1_2_32/output.bin ./output.bin ; then
+cat ./$folderpath/test.m2v | ./dvbtenc $arguments > ./output.bin
+if ./float_cmp ./$folderpath/output.bin ./output.bin 0.25 ; then
   echo dvbtenc same
 else
   echo dvbtenc different
 fi
-#rm ./output.bin
+rm ./output.bin
+
+arguments="-o 8192 -b 7 -c 3 -g 8 -m 4 -a 1 -s 0"
+folderpath="ref_8K_16QAM_2_3_8"
+echo "****************"
+echo "testing 8K mode, bandwidth 7Mhz, coderate 2/3, guardinterval 1/8, modulation 16QAM, alpha 1, outputformat float"
+cat ./$folderpath/test.m2v | ./ed $arguments > ./output_ed_cmp.bin
+if cmp -n $(stat -c %s "./$folderpath/output_ed.bin") ./$folderpath/output_ed.bin ./output_ed_cmp.bin ; then
+  echo ed same
+else
+  echo ed different
+fi
+rm ./output_ed_cmp.bin
+
+cat ./$folderpath/output_ed.bin | ./rs $arguments > ./output_rs_cmp.bin
+if cmp -n $(stat -c %s "./$folderpath/output_rs.bin") -b ./$folderpath/output_rs.bin ./output_rs_cmp.bin; then
+  echo rs same
+else
+  echo rs different
+fi
+rm ./output_rs_cmp.bin
+
+cat ./$folderpath/output_rs.bin | ./oi $arguments > ./output_ci_cmp.bin
+if cmp -n $(stat -c %s "./$folderpath/output_ci.bin") -b ./$folderpath/output_ci.bin ./output_ci_cmp.bin ; then
+  echo ci same
+else
+  echo ci different
+fi
+rm ./output_ci_cmp.bin
+
+cat ./$folderpath/output_ci.bin | ./ce $arguments > ./output_pu_cmp.bin
+if cmp -n $(stat -c %s "./$folderpath/output_pu.bin") -b ./$folderpath/output_pu.bin ./output_pu_cmp.bin ; then
+  echo ce+puncturing same
+else
+  echo ce+puncturing different
+fi
+rm ./output_pu_cmp.bin
+
+cat ./$folderpath/output_pu.bin | ./ii $arguments > ./output_bi_cmp.bin
+if cmp -b -n $(stat -c %s "./$folderpath/output_bi.bin") -b ./$folderpath/output_bi.bin ./output_bi_cmp.bin ; then
+  echo bi same
+else
+  echo bi different
+fi
+rm ./output_bi_cmp.bin
+
+cat ./$folderpath/output_bi.bin | ./si $arguments > ./output_si_cmp.bin
+if cmp -b -n $(stat -c %s "./$folderpath/output_si.bin") -b ./$folderpath/output_si.bin ./output_si_cmp.bin ; then
+  echo si same
+else
+  echo si different
+fi
+rm ./output_si_cmp.bin
+
+cat ./$folderpath/output_si.bin | ./sm $arguments > ./output_ma_cmp.bin
+if cmp -b -n $(stat -c %s "./$folderpath/output_ma.bin") -b ./$folderpath/output_ma.bin ./output_ma_cmp.bin ; then
+  echo ma same
+else
+  echo ma different
+fi
+rm ./output_ma_cmp.bin
+
+cat ./$folderpath/output_ma.bin | ./chan $arguments > ./output_fd_cmp.bin
+if cmp -b -n $(stat -c %s "./$folderpath/output_fd.bin") -b ./$folderpath/output_fd.bin ./output_fd_cmp.bin ; then
+  echo chan same
+else
+  echo chan different
+fi
+rm ./output_fd_cmp.bin
+
+cat ./$folderpath/output_fd.bin | ./ifft $arguments > ./output_cmp.bin
+if ./float_cmp ./$folderpath/output.bin ./output_cmp.bin 0.25 ; then
+  echo ifft same
+else
+  echo ifft different
+fi
+rm ./output_cmp.bin
+
+cat ./$folderpath/test.m2v | ./dvbtenc $arguments > ./output.bin
+if ./float_cmp ./$folderpath/output.bin ./output.bin 0.25 ; then
+  echo dvbtenc same
+else
+  echo dvbtenc different
+fi
+rm ./output.bin
