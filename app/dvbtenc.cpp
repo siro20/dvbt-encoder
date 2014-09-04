b7368222ceeca46bc0e971a0e306e4902e4c68cf
Patrick Rudolph <siro@das-labor.org>
2014-09-04 20:00:35 +0200

    switch to threaded model

--- app/dvbtenc.cpp
+++ app/dvbtenc.cpp
@@ -46,8 +46,87 @@ void print_help_msg()
 	"h		help message\n"
 	"p		print mpegts bitrate\n"
 	"z		print samplerate\n"
+	"x		benchmark\n"
 	);
 	exit(0);
+}
+
+static void read_thread(DVBT_pipe *pin, bool benchmark)
+{
+	struct timeval t1,t2;
+	unsigned long bytecnt = 0;
+	if(benchmark)
+		gettimeofday(&t1, NULL);
+	
+	bool err = false;
+	do{
+		DVBT_memory *mem = new DVBT_memory( 0xFFFF );
+		unsigned int ret, offset;
+		offset = 0;
+		ret = 0;
+		
+		while(offset < mem->size)
+		{
+			ret = fread(mem->ptr+offset, sizeof(char), mem->size-offset, stdin);
+			if(ret <= 0){
+				err = true;
+				break;
+			}
+			offset += ret;
+		}
+		bytecnt += offset;
+		
+		if(offset < mem->size){
+			memset(mem->ptr+offset, 0, mem->size-offset);
+			err = true;
+		}
+		if(!pin->write(mem))
+			err = true;
+	}
+	while(!err);
+
+	if(benchmark){
+		gettimeofday(&t2, NULL);
+		unsigned long diff = t2.tv_sec*1000000+t2.tv_usec-t1.tv_sec*1000000+t1.tv_usec;
+		cerr << "took " << diff << " usec to encode " << bytecnt << " Bytes\nthat's " << (bytecnt*1000000)/(diff*188) << " MPEGts packets/ sec" << endl;
+	}
+	
+	pin->CloseWriteEnd();
+}
+
+static void write_thread(DVBT_pipe *pout)
+{
+	pout->initReadEnd( 0xFFFF );
+
+	bool err = false;
+	do{                
+		DVBT_memory *mem = pout->read();
+		if(!mem || !mem->size)
+		{
+			err = true;
+			break;
+		}
+
+		unsigned int ret, offset;
+		offset = 0;
+		ret = 0;
+		while(offset < mem->size)
+		{
+			ret = fwrite(mem->ptr+offset, sizeof(char), mem->size-offset, stdout);
+			if(ret <= 0){
+				err = true;
+				break;
+			}
+			offset += ret;
+		}
+		//write failed ? just exit
+		if(offset < mem->size){
+			err = true;
+		}
+		delete mem;
+	}
+	while(!err);
+	pout->CloseReadEnd();
 }
 
 /* test function */
@@ -65,6 +144,7 @@ int main(int argc, char *argv[])
 	int outputformat;
 	float gain;
 	int opt;
+	bool benchmark;
 	DVBT_settings *dvbtsettings;
 	bool print_mpegtsbitrate;
 	bool print_samplerate;
@@ -79,12 +159,13 @@ int main(int argc, char *argv[])
 	cellid = 0;
 	gain = 1.0f;
 	outputformat = 4;
-	
+	benchmark = false;
+
 	print_mpegtsbitrate = false;
 	print_samplerate = false;
 	
 	opterr = 0;
-	while ((opt = getopt(argc, argv, "to:b:c:g:m:a:i:s:f:v:zp?h")) != -1)
+	while ((opt = getopt(argc, argv, "xto:b:c:g:m:a:i:s:f:v:zp?h")) != -1)
 	{
 		switch (opt)
 		{
@@ -124,13 +205,16 @@ int main(int argc, char *argv[])
 		case '?':
 			fprintf(stderr, "unknown arg %c\n", optopt);
 			return 1;
-		break;
+			break;
 		case 'p':
 			print_mpegtsbitrate = true;
-		break;
+			break;
 		case 'z':
 			print_samplerate = true;
-		break;
+			break;
+		case 'x':
+			benchmark = true;
+			break;
 		default:
 			print_help_msg();
 		}
@@ -153,8 +237,19 @@ int main(int argc, char *argv[])
 		return 0;
 	}
 
-	DVBT_enc dvbtenc(stdin,stdout,dvbtsettings);
+	DVBT_pipe *pin = new DVBT_pipe("pin");
+	DVBT_pipe *pout = new DVBT_pipe("pout");
+	std::thread wt(write_thread, pout);
+	std::thread rt(read_thread, pin, benchmark);
+	
+	DVBT_enc dvbtenc(pin,pout,dvbtsettings);
+
 	dvbtenc.encode();
+		
+	rt.join();
+	wt.join();
+	delete pin;
+	delete pout;
 	return 0;
 }
 
