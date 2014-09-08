a2e93f0f2c3675c8543927c3405e25ba95e91c28
Patrick Rudolph <siro@das-labor.org>
2014-09-08 19:30:55 +0200

    use B-frames, move logo to fit on 16:9 TV, start bladerf-cli, only supported pixelformat is yuv420p, force CBR

--- app/encoder.sh
+++ app/encoder.sh
@@ -42,9 +42,9 @@ INPUTDIMENSIONY=($(echo $resolution|cut -d 'x' -f2)/16*16)
 fi
 resolution="$((INPUTDIMENSIONX))x$((INPUTDIMENSIONY))"
 
-scalelogox=$(($INPUTDIMENSIONX/10))
-scalelogoy=$(($INPUTDIMENSIONY/8))
-videobitrate=$(($muxrate - 1000000 ))
+scalelogox=$(($INPUTDIMENSIONX/16))
+scalelogoy=$(($INPUTDIMENSIONY/12))
+videobitrate=$(($muxrate - 500000 ))
 echo "MPEGTS muxrate: $muxrate"
 echo "DAC samplerate: $samplerate"
 echo "screen resolution detected: $resolution"
@@ -58,7 +58,7 @@ echo "set samplerate $samplerate" >> /tmp/bladerfcli.tmp
 echo "set bandwidth $(($BANDWIDTH * 1000000 ))" >> /tmp/bladerfcli.tmp 
 
 # Configure gains
-echo "set txvga1 -6" >> /tmp/bladerfcli.tmp 
+echo "set txvga1 -6" >> /tmp/bladerfcli.tmp
 echo "set txvga2 25" >> /tmp/bladerfcli.tmp 
 echo "set rxvga1 20" >> /tmp/bladerfcli.tmp 
 echo "set rxvga2 20" >> /tmp/bladerfcli.tmp 
@@ -70,20 +70,25 @@ echo "tx config file=/tmp/fifo format=bin repeat=0 delay=0 buffers=64" >> /tmp/b
 echo "tx start" >> /tmp/bladerfcli.tmp 
 echo "tx wait" >> /tmp/bladerfcli.tmp 
 
-#bladeRF-cli -s /tmp/bladerfcli.tmp &
+bladeRF-cli -s /tmp/bladerfcli.tmp &
 
+app=
 if [ `which ffmpeg 2>/dev/null` ]; then
-#ffmpeg -i "$1"
-#-b:v $videobitrate \
-ffmpeg \
+app=$( which ffmpeg )
+elif [ `which avconv 2>/dev/null` ]; then
+app=$( which avconv )
+fi
+
+$app \
 -threads 4 \
 -f x11grab -r 10 -s ${resolution[0]} -i :0.0 \
--filter:v "movie=logo.png,scale=$scalelogox:$scalelogoy[watermark];[in][watermark] overlay=main_w-overlay_w*1.1:overlay_h*0.1:,scale=$SCALEX:$SCALEY,drawtext=fontfile='"$FONTFILE"':text='"$SERVICEPROVIDER"':x=(main_w-text_w):y=$scalelogoy-text_h:fontsize=20:fontcolor=white[out]" \
+-filter:v "movie=logo.png,scale=$scalelogox:$scalelogoy[watermark];[in][watermark] overlay=main_w-overlay_w*1.3:overlay_h*0.3:,scale=$SCALEX:$SCALEY,drawtext=fontfile='"$FONTFILE"':text='"$SERVICEPROVIDER"':x=(main_w-text_w*1.3):y=$scalelogoy*1.3-text_h:fontsize=16:fontcolor=white[out]" \
 -codec:a mp2 \
 -b:a 160k \
+-b:v $videobitrate \
+-minrate $videobitrate -maxrate $videobitrate \
 -muxrate $muxrate \
 -codec:v mpeg2video \
--pix_fmt yuv422p \
 -mpegts_original_network_id 0x1122 \
 -mpegts_transport_stream_id 0x3344 \
 -mpegts_service_id 0x5566 \
@@ -91,6 +96,7 @@ ffmpeg \
 -mpegts_start_pid 0x150 \
 -aspect 16:9 \
 -g 12 \
+-bf 2 \
 -mbd rd \
 -trellis 2 \
 -cmp 2 \
@@ -104,8 +110,7 @@ ffmpeg \
 -r 25 \
 -vsync 1 \
 -async 25 \
--f mpegts - 2>ffmpeg.log > /tmp/fifo
-#| \
+-f mpegts - 2>ffmpeg.log | \
 ./dvbtenc -o $FFTSIZE \
 -b $BANDWIDTH \
 -c $CODERATE \
@@ -116,43 +121,6 @@ ffmpeg \
 -v $OVERSAMPLING \
 -s 2 \
 -f $GAIN > /tmp/fifo
-elif [ `which avconv 2>/dev/null` ]; then
-avconv -r 25 -s ${resolution[0]} -i :0.0 \
--filter:v "movie=logo.png,scale=$scalelogox:$scalelogoy[watermark];[in][watermark] overlay=main_w-overlay_w:1,scale=$SCALEX:$SCALEY,drawtext=fontfile='$FONTFILE':text='hackerTV  ':x=(main_w-text_w):y=$scalelogoy+text_h*2:fontsize=20:fontcolor=white[out]" \
--codec:a mp2 \
--b:a 160k \
--muxrate $muxrate \
--threads 4 \
--codec:v mpeg2video \
--mpegts_original_network_id 0x1122 \
--mpegts_transport_stream_id 0x3344 \
--mpegts_service_id 0x5566 \
--mpegts_pmt_start_pid 0x1500 \
--mpegts_start_pid 0x150 \
--metadata service_provider="$SERVICEPROVIDER" \
--metadata service_name="$SERVICENAME" \
--aspect 16:9 \
--mbd rd \
--trellis 2 \
--cmp 2 \
--subcmp 2 \
--qscale 1 \
--y \
--r 25 \
--vsync 1 \
--async 25 \
--f mpegts - 2>avconv.log > /tmp/fifo
-#| \
-./dvbtenc -o $FFTSIZE \
--b $BANDWIDTH \
--c $CODERATE \
--m $MODULATION \
--g $GUARDINTERVAL \
--a 1 \
--i 0 \
--v $OVERSAMPLING \
--s 2 \
--f $GAIN > /tmp/fifo
-fi
+
 rm -f /tmp/bladerfcli.tmp
 rm -f /tmp/fifo
